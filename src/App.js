aab7d363151e2330548fccad9a9533569cfa65b6
bnj997 <bnj997@gmail.com>
2020-07-24 23:18:36 +1000

    pushing unsaved files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Redirect, 
+  Switch 
+} from 'react-router-dom';
+
 import Home from './shared/pages/Home';
 import About from './shared/pages/About';
 import Clients from './shared/pages/Clients';
