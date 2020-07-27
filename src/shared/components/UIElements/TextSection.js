0c542f135110f79d3fbcc75195db9ffdf65b0f42
bnj997 <bnj997@gmail.com>
2020-07-27 14:03:03 +1000

    Initial commit for text sections

---
+++ src/shared/components/UIElements/TextSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './TextSection.css';
+
+
+const TextSection = props => {
+  return (
+    <div>
+      <h1> Hello </h1>
+    </div>
+  );
+};
+
+export default TextSection;
