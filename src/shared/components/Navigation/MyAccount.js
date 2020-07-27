ef5f891b8412ccb946239a6940056c63b31b4e6b
bnj997 <bnj997@gmail.com>
2020-07-27 12:26:42 +1000

    Added content to header

--- src/shared/components/Navigation/MyAccount.js
+++ src/shared/components/Navigation/MyAccount.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import './MyAccount.css';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 const MyAccount = props => {
   return (
-    <React.Fragment>
-      <p className={`${props.drawerIsOpen ? 'header-drawer' : 'header'}`}> My Account | C:  0 </p>
-    </React.Fragment>
+    <div className={`${props.drawerIsOpen ? 'header-drawer' : 'header'}`}>
+      <AccountCircleIcon style={{marginRight: "0.5rem"}} />
+      <p> My Account </p>
+      <p style={{marginLeft: "0.5rem", marginRight: "0.5rem"}}> | </p>
+      <ShoppingCartIcon style={{marginRight: "0.5rem"}}  />
+      <p>: 0 </p>
+    </div>
+
   );
 }
-
 export default MyAccount;
