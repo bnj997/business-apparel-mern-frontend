05e84393accc5338dfb4536d131fee2733f4954f
bnj997 <bnj997@gmail.com>
2020-07-27 12:29:51 +1000

    Initial commit for jumbotron

---
+++ src/shared/components/UIElements/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Jumbotron.css';
+
+const Jumbotron = props => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Jumbotron;
