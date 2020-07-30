94f92e6565cfcf2d7a6ac8bf424fd2dbcfa5792e
bnj997 <bnj997@gmail.com>
2020-07-30 13:05:02 +1000

    Initial commit for authentication page

---
+++ src/shared/pages/Authentication.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import TextSection from '../components/UIElements/TextSection';
+
+const Team = () => {
+  return (
+    <React.Fragment>
+      <TextSection type="center normal" >
+        <h1> hello </h1>
+      </TextSection>
+    </React.Fragment>
+  );
+};
+
+export default Team;
