7ee82b8929cac73a0c2d4820a86e19e7003f9721
bnj997 <bnj997@gmail.com>
2020-08-21 18:03:16 +1000

    Added auth logic into its own auth hook

--- src/App.js
+++ src/App.js
@@ -8,63 +8,12 @@ import {
 
 
 import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/components/hooks/auth-hook';
 import ThisHQ from './admin/pages/ThisHQ'
 import Headquarters from './admin/pages/Headquarters'
 
-let logoutTimer;
-
 const App = () => {
-  const [token, setToken] = useState(false)
-  const [tokenExpirationDate, setTokenExpirationDate] = useState();
-  const [userId, setUserId] = useState(false);
-  const [username, setUsername] = useState(false);
-
-  const login = useCallback((uid, username, token, expirationDate) => {
-    setUserId(uid);
-    setUsername(username);
-    setToken(token);
-    setUserId(uid);
-    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    setTokenExpirationDate(tokenExpirationDate);
-    localStorage.setItem(
-      'userData', 
-      JSON.stringify({
-        userId: uid, 
-        username: username, 
-        token: token, 
-        expiration: tokenExpirationDate.toISOString()}))
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setTokenExpirationDate(null);
-    setUserId(null);
-    setUsername(null);
-    localStorage.removeItem('userData');
-  }, []);
-
-
-  useEffect(() => {
-    if (token && tokenExpirationDate) {
-      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime )
-    } else {
-      clearTimeout(logoutTimer);
-    }
-  }, [token, logout, tokenExpirationDate])
-
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem('userData'));
-    if (
-      storedData && 
-      storedData.token && 
-      new Date(storedData.expiration) > new Date() 
-    ) {
-      login(storedData.userId, storedData.token, storedData.username,  new Date(storedData.expiration));
-    }
-  }, [login])
-
+  const { token, login, logout, userId, username } = useAuth();
 
   let routes;
 
