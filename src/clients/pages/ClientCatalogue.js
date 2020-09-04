0c30bd9aceaa4c6e268fc755eca05255e853a438
bnj997 <bnj997@gmail.com>
2020-09-04 18:21:05 +1000

    Added search function by garment name

--- src/clients/pages/ClientCatalogue.js
+++ src/clients/pages/ClientCatalogue.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useContext, useRef} from 'react';
 import Dashboard from '../../shared/components/PageTemplates/Dashboard'
 import ItemCard from '../UIElements/ItemCard'
-import { Button } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import { useHttpClient } from '../../shared/components/hooks/http-hook';
 import { AuthContext } from '../../shared/context/auth-context';
 import { useSort } from '../utils/sorting';
@@ -9,10 +9,10 @@ import { useSort } from '../utils/sorting';
 const ClientCatalogue = props => {
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const { sortNum, sortString } = useSort();
-
   const auth = useContext(AuthContext);
-
+  const [ baseGarments, setBaseGarments ] = useState([]);
   const [ Garments, setGarments ] = useState([]);
+  const [filter, setFilter] = useState("");
 
   
   useEffect(() => {
@@ -27,36 +27,70 @@ const ClientCatalogue = props => {
           }
         );
         setGarments(sortString(responseData.garments, "name"))
+        setBaseGarments(sortString(responseData.garments, "name"))
       } catch (err) {}
     };
     fetchGarmentsForUser();
   }, [auth.userId, auth.token])
 
 
+  const filterGarments = (garment, filter) => {
+    return garment.filter(function(item) {
+      return item.name.toLowerCase().includes(filter.toLowerCase()) === true
+    });
+  }
+
+
   return (
-    <Dashboard user="client" >
-      <h1>Your Catalogue</h1> 
-      <Button onClick={() => setGarments([...sortString(Garments, "name")])}> Sort by Name</Button>
-      <Button onClick={() => setGarments([...sortString(Garments, "category")])}> Sort by Category</Button> 
-      <Button onClick={() => setGarments([...sortNum(Garments)])}> Sort by Price</Button> 
-
-
-      <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
-        {Garments.map((function(garments) {
-          return (
-            <ItemCard
-              image={garments.image}
-              name={garments.name}
-              category={garments.category}
-              styleNum={garments.styleNum}
-              price={garments.price}
-              colours={garments.colours}
-              sizes={garments.sizes}
-            />
-          )
-        }))}
+    <Dashboard user="client">
+      <div style={{marginLeft: "4rem", marginRight: "4rem", marginTop: "3rem"}}>
+        <h1>Your Catalogue</h1> 
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+          <div>
+            <Button color="primary" onClick={() => setGarments([...sortString(Garments, "name")])}>
+              Sort by Name
+            </Button>
+            <Button color="primary" onClick={() => setGarments([...sortString(Garments, "category")])}>
+              Sort by Category
+            </Button>
+            <Button color="primary" onClick={() => setGarments([...sortNum(Garments)])}>
+              Sort by Price
+            </Button>
+          </div>
+          <TextField 
+            style={{verticalAlign: "baseline", backgroundColor: "white"}} 
+            id="standard-basic"
+            value ={filter}
+            onChange={(e) => {
+              setFilter(e.target.value)
+              setGarments([...filterGarments(baseGarments, e.target.value )])
+            }}
+            variant="outlined" 
+            label="Search" 
+          />
+        </div>
       
-
+      </div>
+     
+      <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
+        {Garments.length > 0 || isLoading ? ( 
+          Garments.map((function(garments) {
+            return (
+              <ItemCard
+                image={garments.image}
+                name={garments.name}
+                category={garments.category}
+                styleNum={garments.styleNum}
+                price={garments.price}
+                colours={garments.colours}
+                sizes={garments.sizes}
+              />
+            )
+            })
+          )
+        ) : (
+          <h2>Search returned no results. Please adjust filter.</h2>
+        )}
       </div>
     </Dashboard>
   );
